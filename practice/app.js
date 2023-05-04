f503e94a1e8a31ef6ac36346edab6569d5249b3f
JoharAbbas-313 <123437515+JoharAbbas-313@users.noreply.github.com>
2023-05-04 04:43:52 -0700

    Add files via upload

---
+++ practice/app.js
@@ -0,0 +1,15 @@
+// function checkAddress(fieldId) {
+//      if (document.getElementById(fieldId).value === "Email address required") {
+//      alert("Email address required.");
+//      }
+//      }
+
+
+
+
+
+
+
+
+
+
